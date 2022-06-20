6d3e74fdc25d198545e217bd65dcbe960390f8ec
mattjkatz <matthewkatz7@gmail.com>
2022-06-20 09:54:56 -0500

    pause function works

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,14 +1,16 @@
+/* Get Our Elements */
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelector("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
-// Functions
+/* Build out functions */
 function togglePlay() {
-  video[video.paused ? "play" : "paused"]();
+  const method = video.paused ? "play" : "pause";
+  video[method]();
 }
 
 // Event Listeners
